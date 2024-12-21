6fa74a6484895c75102c126842ff19389ce328b8
Carl0sL0pez03 <clopez5@udi.edu.co>
2024-12-21 12:09:55 -0500

    Design CreditCardModal. Create hook for validate Visa o MasterCard. Show log credit card.

--- src/components/CreditCardModal.tsx
+++ src/components/CreditCardModal.tsx
@@ -1,21 +1,89 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 import { useForm } from "react-hook-form";
 
-import "./styles/CreditCardModal.css";
 import {
   ICreditCardFormData,
   ICreditCardModalProps,
 } from "./types/creditCardTypes";
+import useCreditCardValidation from "../hook/useCreditCardValidation";
+
+import "./styles/CreditCardModal.css";
 
 function CreditCardModal({ onClose, onSubmit }: ICreditCardModalProps) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    formState: { errors, isValid: validForm },
+    setError,
+    setValue,
+    clearErrors,
+  } = useForm({
+    mode: "all",
+  });
 
-  const handleFormSubmite = (data: ICreditCardFormData) => {
+  const {
+    validateCardNumber,
+    validateExpiryDate,
+    validateCVV,
+    cardType,
+    isValid,
+  } = useCreditCardValidation();
+
+  const handleCardNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let { value } = event.target;
+
+    const cleanedValue = value.replace(/\s+/g, "");
+    const formattedValue = cleanedValue.replace(/(\d{4})/g, "$1 ").trim();
+
+    setValue("cardNumber", formattedValue);
+
+    validateCardNumber(cleanedValue);
+
+    if (!isValid) {
+      setError("cardNumber", {
+        type: "manual",
+        message: "Número de tarjeta inválido.",
+      });
+    } else {
+      clearErrors("cardNumber");
+    }
+  };
+
+  const handleExpiryDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let { value } = event.target;
+
+    const formattedValue = value
+      .replace(/[^0-9/]/g, "")
+      .replace(/^(0[1-9]|1[0-2])$/, "$1/")
+      .replace(/\/\//g, "/");
+
+    setValue("expiryDate", formattedValue);
+
+    if (!validateExpiryDate(formattedValue)) {
+      setError("expiryDate", {
+        type: "manual",
+        message: "Fecha de expiración inválida.",
+      });
+    } else {
+      clearErrors("expiryDate");
+    }
+  };
+
+  const handleCvvChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let { value } = event.target;
+
+    if (!validateCVV(value, cardType!)) {
+      setError("cvv", { type: "manual", message: "CVV inválido." });
+      return;
+    } else {
+      clearErrors("cvv");
+    }
+  };
+
+  const handleFormSubmit = (data: ICreditCardFormData) => {
+    console.log(data);
+
     onSubmit(data);
     onClose();
   };
@@ -23,35 +91,65 @@ function CreditCardModal({ onClose, onSubmit }: ICreditCardModalProps) {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <form onSubmit={handleSubmit(handleFormSubmite)} className="modal-form">
+        <form onSubmit={handleSubmit(handleFormSubmit)} className="modal-form">
           <h2 className="modal-title">Información de pago</h2>
 
           <label htmlFor="cardNumber">Número de la tarjeta</label>
-          <input
-            {...register("cardNumber", { required: true, pattern: /^\d{16}$/ })}
-            name="cardNumber"
-            placeholder="1234 5678 9012 3456"
-          />
+          <div className="input-container">
+            <input
+              {...register("cardNumber", {
+                required: "El número de la tarjeta es obligatorio",
+              })}
+              name="cardNumber"
+              placeholder="1234 5678 9012 3456"
+              onChange={handleCardNumberChange}
+            />
+            <div className="card-logo">
+              {cardType === "VISA" && (
+                <img
+                  src="https://logos-world.net/wp-content/uploads/2020/04/Visa-Emblem.jpg"
+                  alt="Visa"
+                />
+              )}
+              {cardType === "MasterCard" && (
+                <img
+                  src="https://th.bing.com/th/id/OIP.zeRTxpEsrjvlKPWbIlwwagHaEK?rs=1&pid=ImgDetMain"
+                  alt="MasterCard"
+                />
+              )}
+            </div>
+          </div>
+
           {errors?.cardNumber && (
             <p className="error">Introduzca un número de tarjeta válido.</p>
           )}
 
           <label htmlFor="expiryDate">Expiry Date</label>
           <input
-            {...register("expiryDate", { required: true })}
+            {...register("expiryDate", {
+              required: "La fecha de expiración es obligatoria",
+            })}
             name="expiryDate"
+            onChange={handleExpiryDateChange}
           />
-          {errors?.expiryDate && <p>Es un campo requerido.</p>}
+          {errors?.expiryDate && (
+            <p className="error">Introduzca una fecha de expiración valida.</p>
+          )}
 
           <label htmlFor="cvv">CVV</label>
           <input
-            {...register("cvv", { required: true, pattern: /^\d{3}$/ })}
+            {...register("cvv", { required: "El CVV es obligatorio" })}
             name="cvv"
             type="password"
+            onChange={handleCvvChange}
           />
-          {errors?.cvv && <p>Introduzca un CVV válido.</p>}
+          {errors?.cvv && <p className="error">Introduzca un CVV válido.</p>}
 
-          <button type="submit" className="checkout-button">
+          <button
+            type="submit"
+            className="checkout-button"
+            disabled={!validForm}
+          >
             Pagar
           </button>
           <button type="button" className="cancel-button" onClick={onClose}>
